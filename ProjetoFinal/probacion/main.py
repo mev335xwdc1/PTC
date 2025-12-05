41718694e37889c3ba67887dbc9ee53675f3850b
mev335xwdc1 <marianeuseelow@gmail.com>
2025-12-05 22:01:55 +0100

    Add files via upload

---
+++ ProjetoFinal/probacion/main.py
@@ -0,0 +1,19 @@
+import R1
+import R2
+import R3
+import R4
+import R5
+
+def main():
+    print("\n===== INICIAR PROJETO POBLACION =====\n")
+
+    R1.main()
+    R2.main()
+    R3.main()
+    R4.main()
+    R5.main()
+
+    print("\n===== FIM DO PROJETO =====\n")
+
+if __name__ == "__main__":
+    main() 
